494cc8aa2bbdd3812468e072a44d4b10562b14a7
alex-omophub <sdk@omophub.com>
2026-02-28 15:19:14 +0000

    Update version handling

--- src/omophub/_version.py
+++ src/omophub/_version.py
@@ -1,6 +1,11 @@
 """Version information for the OMOPHub SDK."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("omophub")
+except PackageNotFoundError:
+    __version__ = "0.0.0-dev"
 
 
 def get_version() -> str:
